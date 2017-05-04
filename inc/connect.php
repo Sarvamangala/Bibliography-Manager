a19ba1be9b8fceed53b5f3dd874a46cff554c14c
pravin0999 <pravinkumar0999@gmail.com>
2017-05-04 12:08:43 +0100

    updated connection & schema

--- inc/connect.php
+++ inc/connect.php
@@ -1,8 +1,18 @@
 <?php
 try {
+
+	/* credentials for database access  */
+    protected $con;
+    private $host = "us-cdbr-azure-southcentral-f.cloudapp.net";
+    private $user = "bb2888a2afc292";
+    private $pass = "63fbe536";
+    private $db = "db-4244f585-ae0a;Data Source=us-cdbr-azure-southcentral-f.cloudapp.net;User Id=bb2888a2afc292;Password=63fbe536";
+    
 	$user = 'root';
 	$pass = '';
-    $db = new PDO('mysql:host=localhost;dbname=BibliographyManager', $user, $pass);
+    $db = new PDO("mysql:host=$this->host;dbname=$this->db", $this->user, $this->pwd);
+    db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     
     
 } catch (PDOException $e) {
---
+++ inc/schema.php
@@ -0,0 +1,35 @@
+<?php
+include("connect.php");
+
+$createUser = $db -> prepare("CREATE TABLE IF NOT EXISTS 
+	User(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	username VARCHAR(45) DEFAULT NULL,
+	email_id VARCHAR(110) DEFAULT NULL,
+	Password VARCHAR(45),
+	PRIMARY KEY (id))");
+$createUser -> execute();
+
+$createRefs = $db -> prepare("CREATE TABLE IF NOT EXISTS 
+	refs(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	title VARCHAR(145) DEFAULT NULL,
+	author VARCHAR(145) DEFAULT NULL,
+	date_added VARCHAR(145) DEFAULT NULL,
+	date_published VARCHAR(145) DEFAULT NULL,
+	volume VARCHAR(145) DEFAULT NULL,
+	pages INT(11) NOT NULL,
+	user_id INT(11) NOT NULL,
+	abstract VARCHAR(145) DEFAULT NULL,
+	trash INT(4) NOT NULL,
+	PRIMARY KEY (id))");
+$createRefs -> execute();
+
+$createFolder = $db -> prepare("CREATE TABLE IF NOT EXISTS 
+	folders(
+	id INT(11) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(145) DEFAULT NULL,
+	user_id INT(11) NOT NULL,
+	ref_id INT(11) NOT NULL,
+	PRIMARY KEY (id))");
+$createFolder -> execute();
